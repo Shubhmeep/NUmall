c8b5822f311f02d339fcbed157739b6c3e2451fa
Ninja1918 <31992872+Ninja1918@users.noreply.github.com>
2022-04-21 01:02:46 +0530

    API ADDED

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,47 @@
-const express = require('express'); //calling the express library of the node
-const app = express(); //this app will call the func express that we just made
-const cors = require('cors');
-require('dotenv/config'); //use to get some specific values from the .env file we made.
-app.use(cors())
-app.options('*', cors())
-const api = process.env.API_URL; //processing the API_URL variable from the .env file
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const productsRouter = require('./routers/products');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+
+app.use(cors());
+app.options('*', cors())
 
 //middleware
-app.use(express.json()); //bodyparser.json() is depreciated now.
+app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-app.use(`${api}/products`, productsRouter)
 
-mongoose.connect(process.env.CONNECTION_STRING)
-    .then(() => {
-        console.log('DB connection ready')
-    })
-    .catch((err) => {
-        console.log(err)
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
-    })
+const api = process.env.API_URL;
 
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
+//Database
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database'
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+    console.log(err);
+})
 
-app.listen(3000, () => {
-    console.log(api);
-    console.log('server is running now on http://localhost:3000') // when app is running at port 3000 it will print that msg in the terminal
+//Server
+app.listen(3000, ()=>{
+
+    console.log('server is running at http://localhost:3000');
 })
