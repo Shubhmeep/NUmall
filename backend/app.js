63da02312bf4c3a5fb5b27da8f8f1336540a55a8
Shubh Sehgal <shubh.sehgal2506@gmail.com>
2022-04-20 10:07:27 +0530

    Shubh's second commit

--- backend/app.js
+++ backend/app.js
@@ -12,20 +12,46 @@ app.use(express.json()); //bodyparser.json() is depreciated now.
 app.use(morgan('tiny'));
 
 
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: "hair dresser",
-        image: 'some_url',
+const productSchema = mongoose.Schema({
+    //defining the product schema
+    name: String,
+    image: String,
+    countInStock: Number,
+
+
+})
+
+const Product = mongoose.model('Product', productSchema); //defining the product model
+
+app.get(`${api}/products`, async(req, res) => {
+    const productList = await Product.find();
+    if (!productList) {
+        res.status(500).json({
+            success: false
+
+
+        })
     }
-    res.send(product) // this response will be served whenever we visit our localhost
+    res.send(productList) // this response will be served whenever we visit our localhost
 
 })
 
 app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct) // this response will be served whenever we visit our localhost
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+    product.save().then((createdProduct => {
+        res.status(201).json(createdProduct)
+    })).catch((err) => {
+        res.status(500).json({
+            erorr: err,
+            success: false
+
+        })
+    })
+
 
 })
 
