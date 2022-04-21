1b877ead486c33f5712e1f8fbed0be55d77ee83a
Newbie465 <chiragbhatnagar465@gmail.com>
2022-04-21 16:31:33 +0530

    User Authentication Added

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require("./helpers/error-handler");
 
 app.use(cors());
 app.options('*', cors())
@@ -12,6 +14,8 @@ app.options('*', cors())
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
+app.use(authJwt());
+app.use(errorHandler);
 
 
 //Routes
